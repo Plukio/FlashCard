443f770cb49909a187e9ec20b910d51c7b9c7141
Pluk170 <112405935+Pluk170@users.noreply.github.com>
2024-05-01 15:10:24 +0700

    Fix start botton in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="flashcard-container" style="padding: 20px 20px 20px 20px;">
     <div class="flashcard" id="flashcard" onclick="flip()">
         <div class="front">
-            <h2>Why should you use flash cards?</h2>
+            <h2>Why should you use flashcards?</h2>
             <footer style="position: absolute; bottom: 10px; color: #a9a9a9;">
                 Developed by Plug using Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
             </footer>
@@ -12,7 +12,7 @@
         <div class="back" style="justify-content: center; text-align: center;">
             <h3>Flashcards force your brain to actively recall information, strengthening memory.</h3>
             <div class="answer-buttons" style="justify-content: center;">
-            <button onclick="location.href='{{ route('login') }}'" type="submit" class="login-logout-botton">Lets start</a> 
+            <button onclick="location.href='{{ route('login') }}'" type="submit" style = "width: 30%;">Lets start</a> 
             </div>
         </div>
     </div>
